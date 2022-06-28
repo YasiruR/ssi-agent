54c06938b449a32fd58af097c88e68a0d58bd3ac
Yasi <wyasirurathsara@gmail.com>
2022-06-28 15:28:12 +0200

    :construction: removed didexchange

--- agent.go
+++ agent.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-	"github.com/hyperledger/aries-framework-go/pkg/client/didexchange"
 	"github.com/hyperledger/aries-framework-go/pkg/client/issuecredential"
 	"github.com/hyperledger/aries-framework-go/pkg/client/outofbandv2"
 	"github.com/hyperledger/aries-framework-go/pkg/didcomm/common/service"
@@ -13,17 +12,7 @@ import (
 	"github.com/tryfix/log"
 )
 
-type connection struct {
-	id          string
-	ownDID      string
-	receiverDID string
-}
-
 type agent struct {
-	did struct {
-		client *didexchange.Client
-		stream chan service.DIDCommAction
-	}
 	oob struct {
 		client *outofbandv2.Client
 	}
@@ -31,8 +20,7 @@ type agent struct {
 		issuer *issuecredential.Client
 		stream chan service.DIDCommAction
 	}
-	connections map[string]connection
-	logger      log.Logger
+	logger log.Logger
 }
 
 func initAgent(logger log.Logger) *agent {
@@ -46,12 +34,9 @@ func initAgent(logger log.Logger) *agent {
 		logger.Fatal(err)
 	}
 
-	client, clientActions := initDidClient(ctx, logger)
 	issuer, issuerActions := initIssuer(ctx, logger)
 
-	a := agent{logger: logger, connections: map[string]connection{}}
-	a.did.client = client
-	a.did.stream = clientActions
+	a := agent{logger: logger}
 	a.vc.issuer = issuer
 	a.vc.stream = issuerActions
 
@@ -64,24 +49,18 @@ func initAgent(logger log.Logger) *agent {
 	}
 	a.oob.client = oob
 
+	//oobDids, err := didexchange.New(ctx)
+	//if err != nil {
+	//	logger.Fatal(err)
+	//}
+	//oobEvents := make(chan service.DIDCommAction, 5)
+	//
+	//err = oobDids.RegisterActionEvent(oobEvents)
+	//if err != nil {
+	//	logger.Fatal(err)
+	//}
+
 	return &a
-}
-
-func initDidClient(ctx *context.Provider, logger log.Logger) (*didexchange.Client, chan service.DIDCommAction) {
-	client, err := didexchange.New(ctx)
-	if err != nil {
-		logger.Fatal(err)
-	}
-
-	clientActions := make(chan service.DIDCommAction, 1)
-	err = client.RegisterActionEvent(clientActions)
-	if err != nil {
-		logger.Fatal(err)
-	}
-
-	//go service.AutoExecuteActionEvent(clientActions)
-
-	return client, clientActions
 }
 
 func initIssuer(ctx *context.Provider, logger log.Logger) (*issuecredential.Client, chan service.DIDCommAction) {
@@ -117,86 +96,6 @@ func (a *agent) acceptInvitation(inv *outofbandv22.Invitation) error {
 	a.logger.Debug("accepted invitation conn id: ", connID)
 	return nil
 }
-
-//func (a *agent) createInvitation() (*didexchange.Invitation, error) {
-//	inv, err := a.did.client.CreateInvitation("ssi agent invites a user")
-//	if err != nil {
-//		return nil, err
-//	}
-//
-//	connID, err := a.handleInvitation(inv)
-//	if err != nil {
-//		return nil, err
-//	}
-//
-//	conn, err := a.connection(connID)
-//	if err != nil {
-//		return nil, err
-//	}
-//	a.logger.Debug("sender connection details (create invitation): ", conn.State, conn.ConnectionID, conn.MyDID, conn.TheirDID)
-//	return inv, nil
-//}
-
-//func (a *agent) handleInvitation(inv *didexchange.Invitation) (connID string, err error) {
-//	connID, err = a.did.client.HandleInvitation(inv)
-//	if err != nil {
-//		return
-//	}
-//
-//	//conn, err := a.connection(connID)
-//	//if err != nil {
-//	//	a.logger.Error(err)
-//	//}
-//	//a.logger.Debug("receiver connection details (handle invitation): ", conn.State, conn.ConnectionID, conn.MyDID, conn.TheirDID)
-//
-//	return
-//}
-
-//func (a *agent) acceptInvitation(inv *didexchange.Invitation) error {
-//	connID, err := a.handleInvitation(inv)
-//	if err != nil {
-//		return err
-//	}
-//
-//	err = a.did.client.AcceptInvitation(connID, inv.RecipientKeys[0], inv.Label)
-//	if err != nil {
-//		return err
-//	}
-//
-//	//conn, err := a.connection(connID)
-//	//if err != nil {
-//	//	return err
-//	//}
-//
-//	//a.logger.Debug("accepting invitation")
-//	//fmt.Println("conn id: ", conn.ConnectionID)
-//	//fmt.Println("inv id: ", conn.InvitationID)
-//	//fmt.Println("my did: ", conn.MyDID)
-//	//fmt.Println("their did: ", conn.TheirDID)
-//	//fmt.Println("rec keys: ", conn.RecipientKeys)
-//	//fmt.Println("routing keys: ", conn.RoutingKeys)
-//	//fmt.Println("state: ", conn.State)
-//
-//	return nil
-//}
-
-//func (a *agent) connection(connID string) (*didexchange.Connection, error) {
-//	conn, err := a.did.client.GetConnection(connID)
-//	if err != nil {
-//		return &didexchange.Connection{}, errors.New(fmt.Sprintf("%s [%s]", err.Error(), connID))
-//	}
-//
-//	return conn, nil
-//}
-
-//func (a *agent) connectionByState(connID string) (*didexchange.Connection, error) {
-//	conn, err := a.did.client.GetConnectionAtState(connID, "requested")
-//	if err != nil {
-//		return &didexchange.Connection{}, errors.New(fmt.Sprintf("%s [%s]", err.Error(), connID))
-//	}
-//
-//	return conn, nil
-//}
 
 func (a *agent) sendVCOffer(record *connection2.Record) error {
 	out, err := a.vc.issuer.SendOffer(&issuecredential.OfferCredential{}, record)
